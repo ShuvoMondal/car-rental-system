a7d732b0b8fb1d349fb5bbd2a355b673a7daa9d0
ShuvoM <d3v.shuvo@gmail.com>
2026-01-15 22:28:46 +0530

    CREATED:db schema for the users, bookings and vehicles

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,11 +1,32 @@
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, timestamp, integer, date, decimal, pgEnum } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
     id: serial("id").primaryKey(),
-    name: text("name").notNull(),
-    email: text("email").notNull().unique(),
+    username: text("username").notNull().unique(),
     password: text("password").notNull(),
     role: text("role").default("user"),
+    createdAt: timestamp("created_at").defaultNow(),
+    updatedAt: timestamp("updated_at").defaultNow(),
+});
+
+export const bookingStatusEnum = pgEnum("booking_status", ["booked", "completed", "cancelled"]);
+
+export const bookings = pgTable("bookings", {
+    id: serial("id").primaryKey(),
+    userId: integer("user_id").notNull(),
+    vehicleId: integer("vehicle_id").notNull(),
+    days: integer("days").notNull(),
+    status: bookingStatusEnum("status").default("booked"),
+    totalRent: decimal("total_rent").notNull(),
+    createdAt: timestamp("created_at").defaultNow(),
+    updatedAt: timestamp("updated_at").defaultNow(),
+});
+
+export const vehicles = pgTable("vehicles", {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+    description: text("description").notNull(),
+    rentPerDay: decimal("rent_per_day").notNull(),
     createdAt: timestamp("created_at").defaultNow(),
     updatedAt: timestamp("updated_at").defaultNow(),
 });
